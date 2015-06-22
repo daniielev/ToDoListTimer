7b9f1ab49d3e2b3d59a5f762a28696332e775cad
Daniel Muñoz <munnozdaniel@gmail.com>
2015-06-22 14:44:27 -0600

    Implemented the LiveReload extension with Gulp

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# ToDoListTimer
-ToDoListTimer
+# Basic Angular Template
+
+This template uses [Jade](http://jade-lang.com/) for HTML and [Stylus](https://learnboost.github.io/stylus/) for CSS. :)
+
+## Usage
+* Change into the app directory.
+* Run ``npm install`` to install all dependences.
+* Run ``gulp`` to start the localhost server.
+
+Forked from [@ebernabe](https://github.com/ebernabe/ToDoListTimer).
--- gulpfile.js
+++ gulpfile.js
@@ -11,38 +11,50 @@ var nib = require('nib');
 var minify = require('gulp-minify-css');
 var jade = require("gulp-jade");
 
+//Include browser sync
+var browserSync = require('browser-sync').create();
+var reload      = browserSync.reload;
+
 gulp.task("build",["js","styl","jade"]);
 
 gulp.task("js",function(){
-		return browserify({
-			entries:"./app/js/app.js",
-			transform:[babelify,jadeify]
-			})
-		.bundle()
-		.pipe(source("app.js"))
-		.pipe(buffer())
-		.pipe(uglify())
-		.pipe(gulp.dest("./public/js/"))
-	});
+        return browserify({
+            entries:"./app/js/app.js",
+            transform:[babelify,jadeify]
+            })
+        .bundle()
+        .pipe(source("app.js"))
+        .pipe(buffer())
+        .pipe(uglify())
+        .pipe(gulp.dest("./public/js/"));
+    });
 
 
 gulp.task("styl",function(){
    return gulp.src("./app/styl/main.styl")
    .pipe(stylus({use:nib()}))
-   .pipe(concat("app.css"))
+   .pipe(concat("main.css"))
    .pipe(minify())
-   .pipe(gulp.dest("./public/css/"));
+   .pipe(gulp.dest("./public/css/"))
 
-	});
+    });
 gulp.task("jade",function(){
-	return gulp.src("./app/jade/*.jade")
-	.pipe(jade())
-	.pipe(gulp.dest("./public/"))
-	});
+    return gulp.src("./app/jade/*.jade")
+    .pipe(jade())
+    .pipe(gulp.dest("./public/"))
+    });
 
-gulp.task("watch",function(){
-	gulp.watch("./app/jade/*.jade",["jade"]);
-	gulp.watch("./app/styl/*.styl",["styl"]);
-	gulp.watch("./app/js/*.js",["js"]);
-	
-	});
+// Static Server
+gulp.task('serve', ['jade', 'styl', 'js'], function() {
+
+    //reload
+    browserSync.init({
+        server: "./public/"
+    });
+
+    gulp.watch("./app/jade/*.jade",["jade"]).on('change', reload);
+    gulp.watch("./app/styl/*.styl",["styl"]).on('change', reload);
+    gulp.watch("./app/js/*.js",["js"]).on('change', reload);
+});
+
+gulp.task('default', ['serve']);
